82969413276630fa54fa64aef76188898de81ba5
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2020-02-26 13:50:44 -0600

    renamed a function

---
+++ summary.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import pandas
+import os
+
+def main():
+    totalHours = getTotalHours()
+    print("Overall:", totalHours)
+
+def getTotalHours():
+    csvDataFramesList = getCsvDataFramesList()
+    total = 0
+    for dataFrame in csvDataFramesList:
+        hours = getHours(dataFrame)
+        print("Yearly Hours:", hours, "\n")
+        total += hours
+
+    return total
+
+def getCsvDataFramesList():
+    csvPaths = getCsvPaths()
+    dataFrames = []
+    for path in csvPaths:
+        dataFrames.append(pandas.read_csv(path))
+
+    return dataFrames
+
+def getCsvPaths():
+    allPaths = os.listdir(".")
+    csvPaths = []
+    for path in allPaths:
+        if isCsv(path):
+            csvPaths.append(path)
+    
+    return csvPaths
+
+def isCsv(path):
+    return path.split(".")[-1] == "csv"
+
+def getHours(dataFrame):
+    dataFrame = getDurationAsHoursFloat(dataFrame)
+
+    return dataFrame["Duration"].sum()
+
+def getDurationAsHoursFloat(dataFrame):
+    totalRows = dataFrame.shape[0]
+    for i in range(totalRows):
+        duration = dataFrame.loc[i, "Duration"]
+        hoursFloat = getHoursFloat(duration)
+
+        dataFrame.loc[i, "Duration"] = hoursFloat
+
+    return dataFrame
+
+def getHoursFloat(string):
+    # string format: hr:min:s
+    partsAsFloats = [float(part) for part in string.split(":")]
+    return \
+            partsAsFloats[0] \
+            + partsAsFloats[1] / 60 \
+            + partsAsFloats[2] / (60*60)
+
+if __name__ == "__main__":
+    main()
+
