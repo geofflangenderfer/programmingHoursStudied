485c6f4235bf5c92e99735e3365f6779c7b9530d
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2020-02-27 15:03:15 -0600

    improved test case readability in test.py

--- test.py
+++ test.py
@@ -2,7 +2,10 @@
 from hoursStudied import *
 
 def testGetYearlyHoursList():
-    assert abs(sum(getYearlyHoursList()) - 1851.729444444444) < .0001
+    assert abs(
+            sum(getYearlyHoursList()) 
+            - 1851.729444444444
+           ) < .0001
 
 if __name__ == "__main__":
     testGetYearlyHoursList()
